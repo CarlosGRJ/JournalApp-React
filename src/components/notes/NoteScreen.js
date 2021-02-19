8404fc3c5fcc556535148346b9857e4723f94bec
CarlosGRJ <carlosgrj@outlook.com>
2021-02-19 16:37:33 -0600

    Actualizar panel lateral

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useRef } from 'react';
-import { useSelector } from 'react-redux';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 import { useForm } from '../../hooks/useForm';
 
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+  const dispatch = useDispatch();
+
   const { active: note } = useSelector((state) => state.notes);
   const [formValues, handleInputChange, reset] = useForm(note);
   const { body, title } = formValues;
@@ -18,6 +22,10 @@ export const NoteScreen = () => {
     }
   }, [note, reset]);
 
+  useEffect(() => {
+    dispatch(activeNote(formValues.id, { ...formValues }));
+  }, [formValues, dispatch]);
+
   return (
     <div className='notes__main-content'>
       <NotesAppBar />
@@ -28,6 +36,7 @@ export const NoteScreen = () => {
           placeholder='Some awesome title'
           className='notes__title-input'
           autoComplete='off'
+          name='title'
           value={title}
           onChange={handleInputChange}
         />
@@ -35,6 +44,7 @@ export const NoteScreen = () => {
         <textarea
           placeholder='What happened today?'
           className='notes__textarea'
+          name='body'
           value={body}
           onChange={handleInputChange}></textarea>
 
