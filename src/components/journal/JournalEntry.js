f6c37a9ebfe9fd49c366b63851c5e14ee59dc828
CarlosGRJ <carlosgrj@outlook.com>
2021-02-19 10:11:44 -0600

    Nota activa

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,26 +1,45 @@
 import React from 'react';
+import moment from 'moment';
 
-export const JournalEntry = () => {
+import { activeNote } from '../../actions/notes';
+import { useDispatch } from 'react-redux';
+
+export const JournalEntry = ({ id, date, title, body, url }) => {
+  const noteDate = moment(date);
+  const dispatch = useDispatch();
+
+  const handleEntryClick = () => {
+    dispatch(
+      activeNote(id, {
+        date,
+        title,
+        body,
+        url,
+      }),
+    );
+  };
+
   return (
-    <div className='journal__entry pointer'>
-      <div
-        className='journal__entry-picture'
-        style={{
-          backgroundSize: 'cover',
-          backgroundImage:
-            'url(https://images.pexels.com/photos/2670898/pexels-photo-2670898.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)',
-        }}></div>
+    <div className='journal__entry pointer' onClick={handleEntryClick}>
+      {url && (
+        <div
+          className='journal__entry-picture'
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url( url )`,
+          }}></div>
+      )}
+
+      {/* https://images.pexels.com/photos/2670898/pexels-photo-2670898.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940 */}
 
       <div className='journal__entry-body'>
-        <p className='journal__entry-title'>Un nuevo día</p>
-        <p className='journal__entry-content'>
-          Reprehenderit id in duis consectetur deserunt ceniam fugiat
-        </p>
+        <p className='journal__entry-title'>{title}</p>
+        <p className='journal__entry-content'>{body}</p>
       </div>
 
       <div className='journal__entry-date-box'>
-        <span>Monday</span>
-        <h4>28</h4>
+        <span> {noteDate.format('dddd')} </span>
+        <h4>{noteDate.format('Do')}</h4>
       </div>
     </div>
   );
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
+
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+  const { active: note } = useSelector((state) => state.notes);
+  const [formValues, handleInputChange, reset] = useForm(note);
+  const { body, title } = formValues;
+
+  const activeId = useRef(note.id);
+
+  useEffect(() => {
+    if (note.id !== activeId.current) {
+      reset(note);
+      activeId.current = note.id;
+    }
+  }, [note, reset]);
+
   return (
     <div className='notes__main-content'>
       <NotesAppBar />
@@ -12,18 +28,24 @@ export const NoteScreen = () => {
           placeholder='Some awesome title'
           className='notes__title-input'
           autoComplete='off'
+          value={title}
+          onChange={handleInputChange}
         />
 
         <textarea
           placeholder='What happened today?'
-          className='notes__textarea'></textarea>
+          className='notes__textarea'
+          value={body}
+          onChange={handleInputChange}></textarea>
 
-        <div className='notes__image'>
-          <img
-            src='https://images.pexels.com/photos/957040/night-photograph-starry-sky-night-sky-star-957040.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
-            alt='imagen'
-          />
-        </div>
+        {note.url && (
+          <div className='notes__image'>
+            <img
+              src='https://images.pexels.com/photos/957040/night-photograph-starry-sky-night-sky-star-957040.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+              alt='imagen'
+            />
+          </div>
+        )}
       </div>
     </div>
   );
