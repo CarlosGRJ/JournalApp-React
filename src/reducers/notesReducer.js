751d81659e88e5c8b3af2a637eaac4e1b28e6ea2
CarlosGRJ <carlosgrj@outlook.com>
2021-02-20 16:17:17 -0600

    Animaciones

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -29,6 +29,12 @@ export const notesReducer = (state = initialState, action) => {
         },
       };
 
+    case types.notesAddNew:
+      return {
+        ...state,
+        notes: [action.payload, ...state.notes],
+      };
+
     case types.notesLoad:
       return {
         ...state,
@@ -41,6 +47,20 @@ export const notesReducer = (state = initialState, action) => {
         notes: state.notes.map((note) =>
           note.id === action.payload.id ? action.payload.note : note,
         ),
+      };
+
+    case types.notesDelete:
+      return {
+        ...state,
+        active: null,
+        notes: state.notes.filter((note) => note.id !== action.payload),
+      };
+
+    case types.notesLogoutCleaning:
+      return {
+        ...state,
+        active: null,
+        notes: [],
       };
 
     default:
